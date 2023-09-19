bbd3e7f1a1acf1ae47e1174172b6b10ffe402bdb
alexander <alexander@pumarin.se>
2023-09-19 13:31:03 +0200

    basic scanner Example

--- src/com/alexander/scanner/Main.java
+++ src/com/alexander/scanner/Main.java
@@ -5,59 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        boolean isPlaying = true;
-
-        do {
-            int result = scannerNumber();
-
-            if (result > 1000) {
-                isPlaying = false;
-                break;
-            }
-
-            for (int i = 0; i < result; i++) {
-                System.out.println(i);
-                System.out.println("Looping...");
-            }
-
-
-        } while (isPlaying);
-
-    }
-
-        /*
-        Scanner scan = new Scanner(System.in);
-
-        // Input Sequence //
-
-        System.out.println("Please insert a random sentence");
-        scan.nextLine();
-
-        System.out.println("This is unrelated");
-        scan.nextLine();
-        */
-
-    public static int scannerNumber() {
-        Scanner sc = new Scanner((System.in));
-
-        System.out.println("Please insert a number");
-        return sc.nextInt();
-
-
-
-
-
-    }
-    public static String scannerText() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Insert some sentence");
-        String input = sc.nextLine();
+        do {
+
+            System.out.println("Input Number");
+            int result = sc.nextInt();
+
+            System.out.println("You picked: " + result);
+
+        } while (true);
 
 
-
-
-        return input;
     }
 
 }
